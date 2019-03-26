89ea358e0c92caa1dd5c7a695b16af9563e14ba0
akaMackiee <46831591+akaMackiee@users.noreply.github.com>
2019-03-26 18:03:37 +0100

    Added files

---
+++ WordsPerMinute/GetData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.IO;
+using Newtonsoft.Json;
+
+
+namespace WordsPerMinute
+{
+    class GetData : IDataStorage
+    { 
+        public T LoadObject<T>(string key)
+        {
+            string json = File.ReadAllText("key.json");
+            var word = JsonConvert.DeserializeObject<T>(json);
+            return word;
+        }
+    }
+}
---
+++ WordsPerMinute/SweScraper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronWebScraper;
+using Newtonsoft.Json;
+
+namespace WordsPerMinute
+{
+    class SweScraper : WebScraper
+    {
+        string url = "https://larare.at/svenska/moment/lingvistik/vanligaste_orden_i_svenska_spraket.html";
+        public override void Init()
+        {
+            this.LoggingLevel = LogLevel.All;
+            this.Request(url, Parse);
+        }
+
+        public override void Parse(Response response)
+        {
+            foreach(var Word_link in response.Css("ol ol li"))
+            {
+                string Content = Word_link.TextContentClean;
+                var Data = new ScrapedData();
+                Data["language"] = "Swedish";
+                Data["Word"] = Content;
+                Data["Length"] = Content.Length;
+                string json = JsonConvert.SerializeObject(Data, Formatting.Indented);
+                File.AppendAllText("Scrape/WordData.json", json);
+            }
+        }
+
+    }
+}
